6314f3186019323fc25b42481c61eee27cfc8036
plawsyc <1871387250@qq.com>
2021-08-16 09:32:29 +0800

    added code

---
+++ code/00/build/CMakeFiles/Transform.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Transform.dir/main.cpp.obj"
+  "CMakeFiles/Transform.dir/main.cpp.obj.d"
+  "Transform.exe"
+  "Transform.exe.manifest"
+  "Transform.pdb"
+  "libTransform.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Transform.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ code/00/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/10 CG&GAMES/Gameslearning/GamesLearning/code/00
+# Build directory: D:/10 CG&GAMES/Gameslearning/GamesLearning/code/00/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
